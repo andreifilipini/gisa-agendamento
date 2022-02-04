f2e97e32dec7ace628bcc5e105a9d72086ded890
Andrei Filipini <andrei.filipini@sigma.com.br>
2022-02-04 07:52:15 -0300

    conectando mysql

--- src/main/java/com/gisa/gisaagendamento/messages/SchedulingResultConsumer.java
+++ src/main/java/com/gisa/gisaagendamento/messages/SchedulingResultConsumer.java
@@ -2,21 +2,30 @@ package com.gisa.gisaagendamento.messages;
 
 import com.gisa.gisaagendamento.service.ScheduleService;
 import com.gisa.gisacore.dto.BasicTransactionResponseDTO;
+import com.gisa.gisacore.messages.AbstractRabbitConsumer;
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
+@Slf4j
 @Component
-public class SchedulingResultConsumer {
+public class SchedulingResultConsumer extends AbstractRabbitConsumer {
 
     @Inject
     private ScheduleService scheduleService;
 
     @RabbitListener(queues = {"${queue.bookingScheduleResult}"})
-    private void receive(@Payload String body) {
+    protected void receive(@Payload String body) {
+        executeLoggin(body);
+    }
+
+    @Override
+    protected void execute(@Payload String body) {
         Gson gson = new Gson();
         BasicTransactionResponseDTO response = gson.fromJson(body, BasicTransactionResponseDTO.class);
 
@@ -25,5 +34,10 @@ public class SchedulingResultConsumer {
         } else {
             scheduleService.cancel(response.getTransactionId());
         }
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return log;
     }
 }
